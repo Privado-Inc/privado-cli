ee44b8eb68fc5770bf553b56b1e9cd9cffd1245b
Ojaswa Sharma <ojaswa1942@gmail.com>
2023-09-01 23:55:02 +0530

    Revert "Dev (#53)"
    This reverts commit b21fb401b03547654a3ed0d70dde10fb99786920.

--- cmd/scan.go
+++ cmd/scan.go
@@ -70,7 +70,6 @@ func defineScanFlags(cmd *cobra.Command) {
 	scanCmd.Flags().Bool("disable-this-filtering", false, "Experimental: If specified, filtering of flow using 'this filtering algorithm' will be avoided")
 	scanCmd.Flags().Bool("disable-flow-separation-by-data-element", false, "Experimental: If specified, filtering of flow using 'flow separation by data element algorithm' will be avoided")
 	scanCmd.Flags().Bool("disable-2nd-level-closure", false, "Experimental: If specified, 2nd level source derivation will be turned on")
-	scanCmd.Flags().Bool("disable-read-dataflow", false, "Experimental: If specified, read dataflow will be skipped")
 	scanCmd.Flags().Bool("enable-api-display", false, "Experimental: If specified, API display without domain for brute API tagger will be turned on")
 	scanCmd.Flags().Bool("generate-unresolved-name-report", false, "Flag to enable generation unresolved method name reports")
 	scanCmd.Flags().Bool("generate-unfiltered-report", false, "If specified, additionally generates an unfiltered flow report")
@@ -93,7 +92,6 @@ func scan(cmd *cobra.Command, args []string) {
 	disableThisFiltering, _ := cmd.Flags().GetBool("disable-this-filtering")
 	disableFlowSeperationByDataElement, _ := cmd.Flags().GetBool("disable-flow-separation-by-data-element")
 	disable2ndLevelClosure, _ := cmd.Flags().GetBool("disable-2nd-level-closure")
-	disableReadDataflow, _ := cmd.Flags().GetBool("disable-read-dataflow")
 	enableAPIDisplay, _ := cmd.Flags().GetBool("enable-api-display")
 	generateUnresolvedNameReport, _ := cmd.Flags().GetBool("generate-unresolved-name-report")
 	generateUnfilteredReport, _ := cmd.Flags().GetBool("generate-unfiltered-report")
@@ -141,7 +139,7 @@ func scan(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	if !experimentalEnabled && (experimentalJavascriptEnabled || disableRunTimeSemantics || disableThisFiltering || disableFlowSeperationByDataElement || disable2ndLevelClosure || enableAPIDisplay || disableReadDataflow) {
+	if !experimentalEnabled && (experimentalJavascriptEnabled || disableRunTimeSemantics || disableThisFiltering || disableFlowSeperationByDataElement || disable2ndLevelClosure || enableAPIDisplay) {
 		exit(fmt.Sprint(
 			"Experimental features cannot be used without the `--enable-experiments` flag.\n\n",
 			"For more info, run: 'privado help'\n",
@@ -187,10 +185,6 @@ func scan(cmd *cobra.Command, args []string) {
 
 	if disable2ndLevelClosure {
 		commandArgs = append(commandArgs, "-d2lc")
-	}
-
-	if disableReadDataflow {
-		commandArgs = append(commandArgs, "-drd")
 	}
 
 	if enableAPIDisplay {
